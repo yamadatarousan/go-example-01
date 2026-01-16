3c741d92f85ba23cc06b80167664fbb3a46a6ebe
sohei.kakihara <kakiharasou@gmail.com>
2026-01-16 15:43:49 +0900

    不要なディレクトリを削除

--- examples/db/migrations/000009_create_categories_table.down.sql
+++
@@ -1,9 +0,0 @@
--- カテゴリーテーブルのロールバック
-
--- TODOテーブルからカテゴリー外部キーを削除
-DROP INDEX IF EXISTS idx_todos_category;
-ALTER TABLE todos DROP COLUMN IF EXISTS category_id;
-
--- カテゴリーテーブルとインデックスを削除
-DROP INDEX IF EXISTS idx_categories_user_name;
-DROP TABLE IF EXISTS categories;
